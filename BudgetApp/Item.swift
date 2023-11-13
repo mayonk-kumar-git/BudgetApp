6c2c6cf83f7272b00603014b0d706f559d1b925e
mayonk-kumar-git <mayonk.kumar9@gmail.com>
2023-11-13 18:46:26 +0530

    Initial Commit

---
+++ BudgetApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BudgetApp
+//
+//  Created by Mayonk Kumar Behera on 13/11/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
